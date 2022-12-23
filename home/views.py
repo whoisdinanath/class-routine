b28bc95493e1bf8b97458ad3c7ef8cd307005ef9
whoisdinanath <whoisdinanath@gmail.com>
2022-12-23 18:26:01 +0545

    added today's routine section

--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from api.models import Subject
+from api.models import Subject, Level
 from django.db.models import Q
+from datetime import date
 # Create your views here.
 
 
 def home(request):
+    date_today = date.today()
+    day_today = date_today.strftime("%A")
+    levels = Level.objects.all()
+    days = Subject.objects.values('day__day').distinct()
     if request.method == 'GET':
         level = request.GET.get('level')
         sem = request.GET.get('semester')
@@ -16,5 +21,13 @@ def home(request):
             sem = sem.upper()
             subjects = Subject.objects.filter(
                 Q(level__department=level) & Q(day__day=day) & Q(level__semester=sem)).order_by('start_time')
-            context = {'subjects': subjects}
+            context = {'subjects': subjects,
+                       'level': level, 'day': day, 'sem': sem}
+
+        routine_today = Subject.objects.filter(
+            Q(level__department="BEI") & Q(day__day=day_today) & Q(level__semester="II")).order_by('start_time')
+        context['routine_today_subjects'] = routine_today
+        context['levels'] = levels
+        context['days'] = days
+
     return render(request, 'home/home.html', context=context)
