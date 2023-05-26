09274706f4aefe6531190c4560a158040c959704
ihamarch <whoisdinanath@gmail.com>
2023-05-26 17:32:50 +0545

    database secrets managed

--- routine/settings.py
+++ routine/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,13 +82,32 @@ WSGI_APPLICATION = 'routine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if not DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "mssql",
+            "NAME": config("DBNAME"),
+            "USER": config("DBUSER"),
+            "PASSWORD": config("DBPASS"),
+            "HOST": config("DBHOST"),
+            "PORT": "1433",
+            "OPTIONS": {"driver": "ODBC Driver 17 for SQL Server",
+                        },
+        },
+    }
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    CSRF_TRUSTED_ORIGINS = [
+        'https://*.api-acog.azurewebsites.net/',  'http://*.127.0.0.1']
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
